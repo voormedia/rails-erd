62d3fd54dd7206e7c48bdf92a56f3ef0925670de
Masato Ikeda <masato.ikeda@gmail.com>
2013-02-25 23:33:59 +0900

    Make configuration file comma-removable
    Now we don't need to write comma `:` for keys and values.
    See examples/erdconfig.example.

--- lib/rails_erd/config_file.rb
+++ lib/rails_erd/config_file.rb
@@ -14,9 +14,46 @@ module RailsERD
 
     def load
       if File.exists?(USER_WIDE_CONFIG_FILE)  
-        @options = YAML.load_file(USER_WIDE_CONFIG_FILE)
+        YAML.load_file(USER_WIDE_CONFIG_FILE).each do |key, value|
+          key = key.to_sym
+          @options[key] = normalize_value(key, value)
+        end
       end
       @options
+    end
+
+    private
+
+    def normalize_value(key, value)
+      case key
+      # <symbol>[,<symbol>,...] | false
+      when :attributes
+        if value == false
+          return value
+        else
+          # Comma separated string and strings in array are OK.
+          Array(value).join(",").split(",").map { |v| v.strip.to_sym }
+        end
+      
+      # <symbol>
+      when :filetype, :notation, :orientation
+        value.to_sym
+
+      # true | false
+      when :disconnected, :indirect, :inheritance, :markup, :polymorphism, :warn
+        !!value
+      
+      # nil | <string>
+      when :filename, :only, :exclude
+        value.nil? ? nil : value.to_s
+
+      # true | false | <string>
+      when :title
+        value.is_a?(String) ? value : !!value
+
+      else
+        value
+      end
     end
   end
 end
--- test/unit/config_file_test.rb
+++ test/unit/config_file_test.rb
@@ -2,6 +2,10 @@
 require File.expand_path("../test_helper", File.dirname(__FILE__))
 
 class ConfigFileTest < ActiveSupport::TestCase
+  def normalize_value(key, value)
+    RailsERD::ConfigFile.new.send(:normalize_value, key, value)
+  end
+
   test "load_config_gile should return blank hash when USER_WIDE_CONFIG_FILE does not exist." do
     assert_equal RailsERD::ConfigFile.load, {}
   end
@@ -11,6 +15,30 @@ class ConfigFileTest < ActiveSupport::TestCase
     RailsERD::ConfigFile.send :const_set, :USER_WIDE_CONFIG_FILE, 
       File.expand_path("../../../examples/erdconfig.example", __FILE__)
 
-    assert_equal RailsERD::ConfigFile.load, { :attributes => [:content, :foreign_key, :inheritance] }
+    expected_hash = {
+      :attributes     => [:content, :foreign_key, :inheritance], 
+      :disconnected   => true, 
+      :filename       => "erd", 
+      :filetype       => :pdf, 
+      :indirect       => true, 
+      :inheritance    => false, 
+      :markup         => true, 
+      :notation       => :simple, 
+      :orientation    => :horizontal, 
+      :polymorphism   => false, 
+      :warn           => true, 
+      :title          => "sample title", 
+      :exclude        => nil, 
+      :only           => nil
+    }
+    assert_equal expected_hash, RailsERD::ConfigFile.load
+  end
+
+  test "normalize_value should return symbols in an array when key is :attributes and value is a comma-joined string." do
+    assert_equal [:content, :foreign_keys], normalize_value(:attributes, "content,foreign_keys")
+  end
+
+  test "normalize_value should return symbols in an array when key is :attributes and value is strings in an array." do
+    assert_equal [:content, :primary_keys], normalize_value(:attributes, ["content", "primary_keys"])
   end
 end
