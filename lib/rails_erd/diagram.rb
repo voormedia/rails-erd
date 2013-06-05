49448a35579bf50e3b2a0fc08fdb334d0d8a3909
Martin Kammerlander <martin.kammerlander@phlegx.com>
2013-06-05 16:10:33 +0200

    Correct 'No entities found' error. Thanks to espinosa for finding out.

--- lib/rails_erd/diagram.rb
+++ lib/rails_erd/diagram.rb
@@ -151,7 +151,7 @@ module RailsERD
     def filtered_entities
       @domain.entities.reject { |entity|
         options.exclude && entity.model && [options.exclude].flatten.include?(entity.name.to_sym) or
-        options.only && entity.model && ![options.only].flatten.include?(entity.name.to_sym) or
+        !options.only.empty? && entity.model && ![options.only].flatten.include?(entity.name.to_sym) or
         !options.inheritance && entity.specialized? or
         !options.polymorphism && entity.generalized? or
         !options.disconnected && entity.disconnected?
