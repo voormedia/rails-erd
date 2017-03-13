bfeda5349d75527c8a0c83994ce42f0a9858bf1f
David Moodie <davidmoodie12@gmail.com>
2017-03-13 16:47:39 +0000

    Allow user to specify splines type in options

--- lib/rails_erd/diagram/graphviz.rb
+++ lib/rails_erd/diagram/graphviz.rb
@@ -188,6 +188,9 @@ module RailsERD
         # Title of the graph itself.
         graph[:label] = "#{title}\\n\\n" if title
 
+        # Style of splines
+        graph[:splines] = options.splines
+
         # Setup notation options.
         extend self.class.const_get(options.notation.to_s.capitalize.to_sym)
       end
