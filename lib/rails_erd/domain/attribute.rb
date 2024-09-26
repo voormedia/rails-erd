9a031a672d90b7cee33b37fde51898bf0e719e08
Ben Dilley <ben@skillstream.co.uk>
2024-09-26 12:00:09 +0100

    Annotate array types

--- lib/rails_erd/domain/attribute.rb
+++ lib/rails_erd/domain/attribute.rb
@@ -129,6 +129,7 @@ module RailsERD
       # <tt>:boolean, :null => false</tt>:: boolean *
       def type_description
         type.to_s.dup.tap do |desc|
+          desc << "[]" if column.respond_to?(:array?) && column.array?
           desc << " #{limit_description}" if limit_description
           desc << " ∗" if mandatory? && !primary_key? # Add a hair space + low asterisk (Unicode characters)
           desc << " U" if unique? && !primary_key? && !foreign_key? # Add U if unique but non-key
